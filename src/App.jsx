46cf255014ecfa78adadb5b05bf23e8c42ed026e
Osorio3408 <yuliamwow@gmail.com>
2023-09-12 21:38:48 -0600

    se agrego archivo _redirects para evitar el error al recargar la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/home/home";
+import { Home } from "./pages/home/Home";
 
 export const App = () => {
   return (
